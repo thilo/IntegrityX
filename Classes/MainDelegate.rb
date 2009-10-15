9e4f444aa1723b9de3db4aa1aac35cd262343cd4
Thilo Utke <thilo@upstream-berlin.com>
2009-10-16 01:30:28 +0200

    added Project url cycle

--- Classes/MainDelegate.rb
+++ Classes/MainDelegate.rb
@@ -1,5 +1,5 @@
 #
-#  main_controller.rb
+#  MainDelegate.rb
 #  IntegrityX
 #
 #  Created by Thilo on 23.5.09.
@@ -15,23 +15,35 @@ class MainDelegate
 	
 	def	applicationDidFinishLaunching(notification)
 	  @settings
+		@current_index ||= 0
 	  start_call_loop
 	end
 	
 	def start_call_loop
-		timer = NSTimer.scheduledTimerWithTimeInterval 5, target: self, selector: 'request_exception_data:', userInfo: nil, repeats:true
+		timer = NSTimer.scheduledTimerWithTimeInterval 3, target: self, selector: 'request_exception_data:', userInfo: nil, repeats:true
 		timer.fire
 	end
 	
 	def request_exception_data(timer)
 		
 		if @settings["ProjectUrl"] != nil && @settings["SelectedProjects"] != nil && !@settings["SelectedProjects"].empty?
-			server_url_part = @settings["ProjectUrl"] # e.g. "http://builder.integrityapp.com/irc-notifier"
-			project_url_part = @settings["SelectedProjects"].first
-			NSLog "Request update from #{server_url_part + '/' + project_url_part}"
+			server_url_part = @settings["ProjectUrl"] # e.g. "http://ci.atonie.org/irc-notifier"
+			p next_project_url
+			p 'XXXXXXXXXXXXXXX'
+			project_url_part = next_project_url
+			p "Request update from #{server_url_part + '/' + project_url_part}"
 			url = NSURL.URLWithString server_url_part + '/' + project_url_part
 			request = NSURLRequest.requestWithURL url
 			@connection = NSURLConnection.connectionWithRequest(request, delegate: @connection_delegate)
 		end
+	end
+	
+	def next_project_url
+		p 'Determin next url'
+		project_index =  @current_index % @settings["SelectedProjects"].size
+		project_index = 0 if (project_index < 0 || project_index >= @settings["SelectedProjects"].length)
+		@current_index = @current_index + 1
+		
+		@settings["SelectedProjects"][project_index]
 	end
 end
